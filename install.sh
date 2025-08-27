f57424a48992d57028294ea380cb468e79480676
JwpAT <150574239+JwpAT@users.noreply.github.com>
2025-08-27 12:17:33 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -91,6 +91,7 @@ echo 'export PATH="$HOME/.local/bin:$HOME/.config/scripts:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 
 chmod +x ~/.config/scripts/hyprlock-greeter.sh ~/.config/scripts/wireless-menu.sh ~/.config/scripts/waybar-weather.sh ~/.config/scripts/notify.sh ~/.config/scripts/bongocat-cat.sh ~/.config/scripts/wofi-theme.sh ~/.config/scripts/switch-theme
+sudo usermod -a -G input $USER
 
 ~/.config/scripts/switch-theme transparent
 
